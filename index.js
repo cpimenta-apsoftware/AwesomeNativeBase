c80878814be765c425d977550985f3ed58543aaf
cpimenta-apsoftware <cpimenta.apsoftware@gmail.com.br>
2023-03-24 17:35:25 -0300

    chore: react-native-navigation

--- index.js
+++ index.js
@@ -2,8 +2,27 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+// import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import { Navigation } from "react-native-navigation";
 
-AppRegistry.registerComponent(appName, () => App);
+// const app = 
+Navigation.registerComponent(appName, () => App);
+Navigation.events().registerAppLaunchedListener(() => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: appName
+            }
+          }
+        ]
+      }
+    }
+  });
+});
+
+// AppRegistry.registerComponent(appName, () => app);
